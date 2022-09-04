edb482c2831bce9b03145f5f812fa89727358297
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2022-09-04 10:44:35 -0600

    created button that hides start area

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+var startArea = document.getElementById("start");
+var startBtn = document.getElementById("startBtn");
+    startBtn.addEventListener("click", function(){
+        startArea.style.display = "none";
+    })
+// Questions:
+//     "What is the syntax for an if statement?"
+//     "Which javascript data type is not an actual data type?"
+//     "What is the correct syntax to create an array?"
+//     "Which function replaces the text of the element called 'info'?"
+//     "Which code creates a loop that will run as many times as there are entries in the array?"
+
