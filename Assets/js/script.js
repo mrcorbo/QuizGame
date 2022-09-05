9b3e17586e1d59542a7fccb48f7f4136102e7141
mrcorbo <109931379+mrcorbo@users.noreply.github.com>
2022-09-05 15:51:19 -0600

    Fleshed out answer logic, stopped timer, created game over implementation

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,24 +2,28 @@ var startArea = document.getElementById("start");
 var quizArea = document.getElementById("quizQs");
 var startBtn = document.getElementById("startBtn");
 var timerEl = document.getElementById("timer");
-var timeLeft = 10; 
+var timeLeft = 300;
+var response = document.getElementById("response");
+var correct = 0;
+var timer;
+var finishArea = document.getElementById("quizEnd");
 
 startBtn.addEventListener("click", function () {
     startArea.style.display = "none";
     quizArea.style.display = "block";
-    displayQuestion();
     startTimer();
+    displayQuestion();
 });
-function startTimer(){
-    var timer = setInterval(function(){
-        if (timeLeft>0){
+function startTimer() {
+    timer = setInterval(function () {
+        if (timeLeft > 0) {
             timeLeft--;
-            timerEl.innerText="Time left: " + timeLeft;
-        }else {
+            timerEl.innerText = "Time left: " + timeLeft;
+        } else {
             clearInterval(timer);
         }
-    },1000);
-}
+    }, 1000);
+};
 var questions = [
     {
         questionText: "What is the syntax for an if statement?",
@@ -28,26 +32,60 @@ var questions = [
     },
     {
         questionText: "Which javascript data type is not an actual data type?",
+        choices: ["boolean", "number", "alpha", "string"],
+        answer: "alpha"
+    },
+    {
+        questionText: "What is the correct syntax to create an array?",
+        choices: ["var array = []", "var array() =", "var array = {}", "var array:"],
+        answer: "var array = []"
+    },
+    {
+        questionText: "Which function replaces the text of the element called 'info'?",
+        choices: ["function(info) =", "replaceText.info =", "info.function()", "info.textContent ="],
+        answer: "info.textContent ="
     }
-]
+];
 var questionNum = 0;
 function displayQuestion() {
     var qText = document.getElementById("questionText");
     qText.innerText = questions[questionNum].questionText;
     var choices = document.getElementById("choices");
     choices.innerHTML = "";
-    for (var i=0; i<questions[questionNum].choices.length; i++){
+    for (var i = 0; i < questions[questionNum].choices.length; i++) {
         var button = document.createElement("button");
         button.innerText = questions[questionNum].choices[i];
+        button.addEventListener("click", function (event) {
+            console.log(questionNum,questions.length)
+            var choice = event.target.innerText;
+            console.log(choice);
+            if (choice != questions[questionNum].answer) {
+                timeLeft = timeLeft - 30;
+                response.innerText = "Incorrect answer, you've been penalized 30 seconds."
+            } else {
+                response.innerText = "Correct!"
+                correct++;
+            }
+            questionNum++;
+            if (questionNum==questions.length){
+                console.log("inside");
+                quizArea.style.display="none";
+                clearInterval(timer);
+                gameOver();
+                return;
+            }
+            displayQuestion();
+        });
         choices.appendChild(button);
     }
-
 };
-
-// Questions:
-//
-//
-//     "What is the correct syntax to create an array?"
-//     "Which function replaces the text of the element called 'info'?"
-//     "Which code creates a loop that will run as many times as there are entries in the array?"
-
+function gameOver(){
+    var congrats = document.createElement("h3");
+    congrats.innerText = "Congratulations, you scored: " + (correct*25 + timeLeft);
+    finishArea.appendChild(congrats);
+    var initials = document.createElement("input");
+    finishArea.appendChild(initials);
+    var saveBtn = document.createElement("button");
+    saveBtn.innerText = "Save score"
+    finishArea.appendChild(saveBtn);
+}
